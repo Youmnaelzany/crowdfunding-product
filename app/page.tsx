d2d98dceb220d1ed7446788eedffd3e9d67b5850
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-12-16 02:44:19 +0200

    feat: add ProgressBar component and update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import MonitorRise from "@/components/MonitorRise";
+import ProgressBar from "@/components/ProgressBar";
 
 export default function Page() {
   return (
-    <main className="mx-auto">
-      <section className="flex items-center justify-center relative top-54 lg:top-58 md:top-50">
+    <main className="mx-auto relative top-54 lg:top-58 md:top-50">
+      <section className="flex items-center flex-col gap-6 justify-center ">
         <MonitorRise />
+        <ProgressBar />
       </section>
     </main>
   );
---
+++ components/ProgressBar.tsx
@@ -0,0 +1,8 @@
+const ProgressBar = () => {
+  return (
+    <div className="w-[327px] md:w-[672px] lg:w-[730px] flex flex-col items-center justify-center bg-white rounded-lg md:h-[210px] h-[412px] p-12 lg:h-[212px]">
+      ProgressBar
+    </div>
+  );
+};
+export default ProgressBar;
